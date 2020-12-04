85cac602221e8e7edb92a2c8a3486cc4d458e83d
junjangsee <junjang.see@gmail.com>
2020-12-04 14:03:31 +0900

    [#3]style: 유저 리스트 컴포넌트 구현

---
+++ src/containers/userListContainer/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserList from '../../components/contact/userList';
+
+const UserListContainer = () => {
+  return <UserList />;
+};
+
+export default UserListContainer;
